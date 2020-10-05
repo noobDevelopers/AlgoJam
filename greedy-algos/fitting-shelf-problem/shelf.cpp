f8a16e8933e40fbc671831ed66913f5feb64abd0
Apoorv Mishra <55011904+APOORVMISHRA21@users.noreply.github.com>
2020-10-05 07:00:07 +0530

    Update shelf.cpp

--- greedy-algos/fitting-shelf-problem/shelf.cpp
+++ greedy-algos/fitting-shelf-problem/shelf.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+void minSpace(int wall, int m, int n)
+{
+  /* algo for solving the issue*/
+  
+}
 
+  
+// Driver code 
+int main() 
+{ 
+    int wall = 24, m = 3, n = 5; 
+    minSpace(wall, m, n);
+  
+    return 0; 
+} 
